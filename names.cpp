f58b7d60bbc720d4a368234fcb58bfcedfb5dbb6
anumayagrawal12 <anumayagrawal@gmail.com>
2025-02-07 23:35:28 +0530

    new cpp modified

--- names.cpp
+++ names.cpp
@@ -1,1 +1,13 @@
- 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    for (int i0; i < 10; i++)
+    {
+        x++;
+    }
+    cout << x << endl;
+    return 0;
+}
